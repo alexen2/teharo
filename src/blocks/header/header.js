c283a33576e8f7f17f4e2b8a5b71bdc0bf76f2f3
Andrey <staforg@mail.ru>
2020-05-04 11:45:08 +0500

    Add style guide

---
+++ src/blocks/header/header.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+	$('.header__btn').click(function() {
+		$('.header__mobile').toggleClass('header__mobile--open');
+		$('body').toggleClass('overflow-heidden ');
+	});
+
+	$('.header-mobile__btn').click(function() {
+		$('.header__mobile').toggleClass('header__mobile--open');
+		$('body').toggleClass('overflow-heidden ');
+	});
+
+	let scrolled = 25;
+
+	// changeScrolled();
+
+
+$(window).scroll(function(){
+	let $this = $(this),
+		st = $this.scrollTop();
+
+	menuScrollDesktop(st);
+});
+
+$(window).resize(function(){
+	let st = $(window).width();
+
+	menuDesktopShow(st);
+});
+
+function menuScrollDesktop(st) {
+	if(st > scrolled){
+		$('body').addClass('scrolled');
+	}else{
+		$('body').removeClass('scrolled');
+	}
+};
+
+function menuDesktopShow(st) {
+	if(st > 991) {
+		if($('.overflow-heidden').length) {
+			$('.header__mobile').toggleClass('header__mobile--open');
+			$('body').toggleClass('overflow-heidden');
+		}
+	}
+};
+
+function changeScrolled() {
+	scrolled = 0;
+};
+
+});
---
+++ src/blocks/slider/slider.js
@@ -0,0 +1,10 @@
+let slider = $('.slider').slick({
+  arrows: false,
+	dots: true,
+	dotsClass: 'slider__dots',
+	infinite: false,
+	speed: 300,
+  fade: true,
+  cssEase: 'linear',
+  slidesToScroll: 1
+});
